3d34f3c0d33a956fa25511296f2d327b5bb007d2
Ayrton Sousa <ayrtontisolution@hotmail.com.br>
2018-06-11 19:21:34 -0300

    issue4 - [Criar geradores de Inimigos]
    Gerador de inimigos criados, foi criado um pequeno vetor que guarda alguns poucos inimigos, podendo aumentá-lo depois.

---
+++ Assets/Scripts/Game Objects/EnemyCreatorScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreatorScript : MonoBehaviour {
+
+
+    public GameObject enemy;
+    Transform thisTransform;
+    public GameObject[] enemies =  new GameObject[4];
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(CreateEnemy());
+        thisTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator CreateEnemy() {
+        while (true)
+        {
+            GameObject e = Instantiate(enemies[Random.Range(0,4)],new Vector2(Random.Range(-6, 10), gameObject.transform.position.y),gameObject.transform.rotation);
+           
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Game Objects/EnemySpaceshipScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpaceshipScript : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public int speed;
+   
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * speed *Time.deltaTime);
+    }
+}
