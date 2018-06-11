f99c4f09801e72fbd0f3059b563e545b89a78046
Ayrton Sousa <ayrtontisolution@hotmail.com.br>
2018-06-11 19:49:13 -0300

    #3 - Delimitar Limites

--- Assets/Scripts/Game Objects/EnemySpaceshipScript.cs
+++ Assets/Scripts/Game Objects/EnemySpaceshipScript.cs
@@ -6,14 +6,29 @@ public class EnemySpaceshipScript : MonoBehaviour {
 
     Rigidbody2D rb;
     public int speed;
-   
+    public GameObject player;
+    Transform tr;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
+        tr = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        tr.LookAt(player.transform);
+    }
+
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * speed *Time.deltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("killerWall")) {
+            Destroy(gameObject);
+        }
+    }
+
 }
